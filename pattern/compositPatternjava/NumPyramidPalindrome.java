d35c5206f4813a3d3d0d1d9d50427a1589b68ce9
Ibrahim Hussain <ibrahimhussain0928@gmail.com>
2025-11-22 16:35:43 +0530

    Update NumPyramidPalindrome.java

--- pattern/compositPatternjava/NumPyramidPalindrome.java
+++ pattern/compositPatternjava/NumPyramidPalindrome.java
@@ -28,3 +28,36 @@ public class NumPyramidPalindrome {
 
     }
 }
+
+
+
+//2nd type pelindrome...
+
+
+import java.util.Scanner;
+public class Basic2Darray {
+    public static void main(String[] args) {
+
+        //input condition.....
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the n");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            //spaces
+            for(int j = 1; j <= n-i; j++){
+                System.out.print(" " + " ");
+            }
+            //1st half number
+            for(int j = i; j >= 1; j--){
+                System.out.print(j + " ");
+            }
+
+            //2nd half number
+            for(int j = 2; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
