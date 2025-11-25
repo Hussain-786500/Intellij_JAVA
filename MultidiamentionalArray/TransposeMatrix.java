5e991248f060b2cd9b49ce35266bc29152cdc4fc
Ibrahim Hussain <ibrahimhussain0928@gmail.com>
2025-11-25 12:23:22 +0530

    Update TransposeMatrix.java

--- MultidiamentionalArray/TransposeMatrix.java
+++ MultidiamentionalArray/TransposeMatrix.java
@@ -44,3 +44,55 @@ public class TransposeMatrix {
         }
     }
 }
+
+//3rd method through input Scanner...
+
+import java.util.Scanner;
+
+public class PascalTriangle {
+
+    // Method to print 2D array
+    public static void print(int[][] arr){
+        int n = arr.length;
+        int m = arr[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of row");
+        int n = sc.nextInt();
+        System.out.println("Enter number of cols");
+        int m = sc.nextInt();
+
+        int[][] arr = new int[n][m];
+
+        //transpose print.....Input array elements
+        System.out.println("Enter your matrix");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Original Array");
+        print(arr);
+
+        System.out.println("Transpose Array");
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
