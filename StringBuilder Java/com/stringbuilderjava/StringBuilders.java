7f209491c1fb6a0651fb8afe4f737d3610c6831d
Ibrahim Hussain <ibrahimhussain0928@gmail.com>
2025-12-09 12:53:09 +0530

    YEAS

--- StringBuilder Java/com/stringbuilderjava/StringBuilders.java
+++
@@ -1,9 +0,0 @@
-package com.stringbuilderjava;
-
-public class StringBuilders {
-
-    public static void main(String[] args) {
-        StringBuilder name = new StringBuilder("HUSSAIN");
-        System.out.println(name.reverse());
-    }
-}
